2a637c2ea51119cff3715d2da239cc673442775e
omernaeem78601 <omernaeem10@gmail.com>
2023-12-20 14:57:14 +0500

    update

--- crm-web/src/App.js
+++ crm-web/src/App.js
@@ -1,14 +1,23 @@
-import LandingPage from "./components/LandingPage";
-
+import { Home } from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Login from "./pages/Login";
+import NavHeader from "./components/Navbar/NavHeader";
+import Footer from "./components/Footer";
+import SignUp from "./pages/SignUp";
 
 function App() {
- 
   return (
-    <>
-    <LandingPage/>
-    
-    </>
-  
+    <div>
+      <NavHeader />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+      <ToastContainer theme="dark" />
+    </div>
   );
 }
 
---
+++ crm-web/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import FourColumns from "../components/FourColumns";
+import Deals from "../components/Deals";
+import BestSale from "../components/BestSale";
+import PopularProducts from "../components/PopularProducts";
+import Banner from "../components/Banner";
+import Category from "../components/Category";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <main className="main">
+        <Header />
+        {/*End hero slider*/}
+        <Category />
+        {/*End category slider*/}
+        <Banner />
+        {/*End banners*/}
+        <PopularProducts />
+        {/*Products Tabs*/}
+        <BestSale />
+        {/*End Best Sales*/}
+        <Deals />
+        {/*End Deals*/}
+        <FourColumns />
+        {/*End 4 columns*/}
+      </main>
+    </>
+  );
+};
+
+export { Home };
